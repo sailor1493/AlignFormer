bdf44bb113277f5a03eeb1051389fffd0820f5b8
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-11-01 00:51:04 +0000

    add ecfnet loss

--- basicsr/losses/losses.py
+++ basicsr/losses/losses.py
@@ -39,6 +39,44 @@ def modified_mse_loss(pred, target, eps=1e-3):
     return ((pred - target) / (weight + eps)) ** 2
 
 
+
+@LOSS_REGISTRY.register()
+class ECFNetLoss(nn.Module):
+    """L1 (mean absolute error, MAE) loss.
+
+    Args:
+        loss_weight (float): Loss weight for L1 loss. Default: 1.0.
+        reduction (str): Specifies the reduction to apply to the output.
+            Supported choices are 'none' | 'mean' | 'sum'. Default: 'mean'.
+    """
+
+    def __init__(self, loss_weight=1.0, lambda_value=0.5, reduction="mean"):
+        super(ECFNetLoss, self).__init__()
+        if reduction not in ["none", "mean", "sum"]:
+            raise ValueError(
+                f"Unsupported reduction mode: {reduction}. "
+                f"Supported ones are: {_reduction_modes}"
+            )
+
+        self.loss_weight = loss_weight
+        self.lambda_value = lambda_value
+        self.reduction = reduction
+
+    def forward(self, pred, target, weight=None, **kwargs):
+        """
+        Args:
+            pred (Tensor): of shape (N, C, H, W). Predicted tensor.
+            target (Tensor): of shape (N, C, H, W). Ground truth tensor.
+            weight (Tensor, optional): of shape (N, C, H, W). Element-wise
+                weights. Default: None.
+        """
+        l1_loss_value = l1_loss(pred, target, weight, reduction=self.reduction)
+        charbonnier_loss_value = charbonnier_loss(pred, target, weight, reduction=self.reduction)
+            
+        return self.loss_weight * (l1_loss_value + self.lambda_value * charbonnier_loss_value)
+
+
+
 @LOSS_REGISTRY.register()
 class L1Loss(nn.Module):
     """L1 (mean absolute error, MAE) loss.
