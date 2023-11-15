49a9eab11cbae0c28aabf455000e2fea19ef8cfe
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-11-15 12:39:52 +0900

    you manually install mmcv

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Wed Nov 15 12:03:53 2023
+# TIME: Wed Nov 15 12:39:01 2023
 __version__ = '1.3.4.1'
-__gitsha__ = 'ccf23d3'
+__gitsha__ = '7d1beaf'
 version_info = (1, 3, 4, 1)
